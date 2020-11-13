05ac0118e3a88ca22104c311dbd07066b525f168
Matthias Friedrich <matze.friedrich@googlemail.com>
2020-11-13 12:22:08 +0100

    Adds new overloads for OnExecute

--- src/System.CommandLine.Extensions/CommandWrapper.cs
+++ src/System.CommandLine.Extensions/CommandWrapper.cs
@@ -43,6 +43,10 @@ namespace System.CommandLine.Extensions
         public void OnExecute<T1, T2, T3, T4>(Func<T1, T2, T3, T4, Task<int>> execute) => this.command.Handler = CommandHandler.Create(execute);
         public void OnExecute<T1, T2, T3, T4, T5>(Func<T1, T2, T3, T4, T5, Task<int>> execute) => this.command.Handler = CommandHandler.Create(execute);
         public void OnExecute<T1, T2, T3, T4, T5, T6>(Func<T1, T2, T3, T4, T5, T6, Task<int>> execute) => this.command.Handler = CommandHandler.Create(execute);
+        public void OnExecute<T1, T2, T3, T4, T5, T6, T7>(Func<T1, T2, T3, T4, T5, T6, T7, Task<int>> execute) => this.command.Handler = CommandHandler.Create(execute);
+        public void OnExecute<T1, T2, T3, T4, T5, T6, T7, T8>(Func<T1, T2, T3, T4, T5, T6, T7, T8, Task<int>> execute) => this.command.Handler = CommandHandler.Create(execute);
+        public void OnExecute<T1, T2, T3, T4, T5, T6, T7, T8, T9>(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, Task<int>> execute) => this.command.Handler = CommandHandler.Create(execute);
+        public void OnExecute<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, Task<int>> execute) => this.command.Handler = CommandHandler.Create(execute);
 
         public CommandWrapper Argument<T>(string name, string description = null)
         {
